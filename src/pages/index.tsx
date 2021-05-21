683339152a47901b96cef8e8ec302715aa03e35b
Lucas Santos <l.duarte.mk@gmail.com>
2021-05-21 16:00:16 -0300

    dinamic header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,14 @@
+import Head from 'next/head'
+
 export default function Home() {
-  return <h1>Hello world</h1>;
+  return (
+      <>
+        <Head>
+          <title>Inicio | ig.news</title>
+        </Head>
+        <h1>
+          Hello world
+        </h1>
+      </> 
+    );
 }
