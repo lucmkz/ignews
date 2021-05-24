1eed9fb1b45f7d5099d3fc1860f8dbe9555bd366
Lucas Santos <l.duarte.mk@gmail.com>
2021-05-24 17:47:19 -0300

    revalidate i time per day

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 
 import Head from 'next/head'
 import { SubscribeButton } from '../components/SubscribeButton';
@@ -36,7 +36,7 @@ export default function Home({ product }: HomeProps) {
     );
 }
 
-export const getServerSideProps: GetServerSideProps = async() => {
+export const getStaticProps: GetStaticProps = async() => {
   const price = await stripe.prices.retrieve('price_1Iujt7FTRa8UouACFGw4DGnb')
 
   const product = {
@@ -51,6 +51,7 @@ export const getServerSideProps: GetServerSideProps = async() => {
   return {
     props: {
       product,
-    }
+    },
+    revalidate: 60 * 60 * 24 // 24 hours
   }
 }
