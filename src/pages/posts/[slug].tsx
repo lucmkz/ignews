d081c93391243648ffbbb81978fefb86e0a90055
Lucas Santos <l.duarte.mk@gmail.com>
2021-05-26 23:42:14 -0300

    create previwe post

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -41,8 +41,7 @@ export const getServerSideProps: GetServerSideProps = async ({ req, params }) =>
     const session = await getSession({ req })
     const { slug } = params
 
-    console.log(session)
-    if (!session.activeSubscription) {
+    if (!session?.activeSubscription) {
         return {
             redirect: {
                 destination: '/',
