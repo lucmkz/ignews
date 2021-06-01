d1e900bbf772e767ec5d54063ffa4f29abb2fb2d
Lucas Santos <l.duarte.mk@gmail.com>
2021-06-01 20:50:22 -0300

    first test added

--- src/components/ActiveLink/ActiveLink.spec.tsx
+++ src/components/ActiveLink/ActiveLink.spec.tsx
@@ -1,10 +1,34 @@
 import { render } from "@testing-library/react";
 import { ActiveLink } from ".";
 
-test("active link renders correctly", () => {
-  render(
-    <ActiveLink href="/" activeClassName="active">
-      <a>home</a>
-    </ActiveLink>
-  );
+jest.mock("next/router", () => {
+  return {
+    useRouter() {
+      return {
+        asPath: "/",
+      };
+    },
+  };
 });
+
+describe('ActiveLink Component',() => {
+  it("renders correctly", () => {
+    const { debug, getByText } = render(
+      <ActiveLink href="/" activeClassName="active">
+        <a>home</a>
+      </ActiveLink>
+    );
+  
+    expect(getByText("home")).toBeInTheDocument();
+  });
+  
+  it("adds active class if the link is the same as href", () => {
+    const { debug, getByText } = render(
+      <ActiveLink href="/" activeClassName="active">
+        <a>home</a>
+      </ActiveLink>
+    );
+  
+    expect(getByText("home")).toHaveClass("active");
+  });
+})
